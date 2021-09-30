80d0664336ffbba9759b7bc5b8fdf6cbf9ec6ef7
Ilya Denisov <i.denisovs@gmail.com>
2021-09-30 14:54:05 +0300

    package.json - dependency updates.

--- dist/index.js
+++ dist/index.js
@@ -8,104 +8,20 @@
 	else
 		root["yetAnotherDuration"] = factory();
 })(typeof self !== 'undefined' ? self : this, function() {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.ts");
-/******/ })
-/************************************************************************/
-/******/ ({
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/duration/calculate-months.ts":
 /*!******************************************!*\
   !*** ./src/duration/calculate-months.ts ***!
   \******************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return calculateMonths; });
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ calculateMonths)
+/* harmony export */ });
 var DAY = 1000 * 60 * 60 * 24;
 var ODD_MONTH_DAYS = 31;
 var ODD_MONTH_DAYS_MS = ODD_MONTH_DAYS * DAY;
@@ -145,12 +61,12 @@ function calculateMonths(duration) {
 /*!***************************************!*\
   !*** ./src/duration/get-unit-list.ts ***!
   \***************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return getUnitList; });
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ getUnitList)
+/* harmony export */ });
 function getUnitList(includeWeeks) {
     if (includeWeeks === void 0) { includeWeeks = false; }
     if (includeWeeks) {
@@ -168,11 +84,12 @@ function getUnitList(includeWeeks) {
 /*!*******************************!*\
   !*** ./src/duration/index.ts ***!
   \*******************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
 /* harmony import */ var _trim_zero_values__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./trim-zero-values */ "./src/duration/trim-zero-values.ts");
 /* harmony import */ var _make_object__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./make-object */ "./src/duration/make-object.ts");
 /* harmony import */ var _get_unit_list__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./get-unit-list */ "./src/duration/get-unit-list.ts");
@@ -185,12 +102,12 @@ var Duration = /** @class */ (function () {
     function Duration(duration, options) {
         this.duration = duration;
         this.options = options;
-        this.units = Object(_get_unit_list__WEBPACK_IMPORTED_MODULE_2__["default"])(this.options.calculateWeeks);
-        Object(_process_units_option__WEBPACK_IMPORTED_MODULE_3__["default"])(this.units, this.options);
+        this.units = (0,_get_unit_list__WEBPACK_IMPORTED_MODULE_2__["default"])(this.options.calculateWeeks);
+        (0,_process_units_option__WEBPACK_IMPORTED_MODULE_3__["default"])(this.units, this.options);
     }
     Duration.prototype.toString = function () {
-        var obj = Object(_make_object__WEBPACK_IMPORTED_MODULE_1__["default"])(this.duration, this.units);
-        Object(_trim_zero_values__WEBPACK_IMPORTED_MODULE_0__["default"])(this.units, obj, this.options);
+        var obj = (0,_make_object__WEBPACK_IMPORTED_MODULE_1__["default"])(this.duration, this.units);
+        (0,_trim_zero_values__WEBPACK_IMPORTED_MODULE_0__["default"])(this.units, obj, this.options);
         var result = [];
         for (var _i = 0, _a = this.units; _i < _a.length; _i++) {
             var unit = _a[_i];
@@ -200,7 +117,7 @@ var Duration = /** @class */ (function () {
         return result.join(' ');
     };
     Duration.prototype.toTimeSpan = function () {
-        var obj = Object(_make_object__WEBPACK_IMPORTED_MODULE_1__["default"])(this.duration, this.units);
+        var obj = (0,_make_object__WEBPACK_IMPORTED_MODULE_1__["default"])(this.duration, this.units);
         var result = [];
         for (var _i = 0, _a = this.units; _i < _a.length; _i++) {
             var unit = _a[_i];
@@ -215,7 +132,7 @@ var Duration = /** @class */ (function () {
         return result.join(':');
     };
     Duration.prototype.toISO8601 = function () {
-        var obj = Object(_make_object__WEBPACK_IMPORTED_MODULE_1__["default"])(this.duration, Object(_get_unit_list__WEBPACK_IMPORTED_MODULE_2__["default"])());
+        var obj = (0,_make_object__WEBPACK_IMPORTED_MODULE_1__["default"])(this.duration, (0,_get_unit_list__WEBPACK_IMPORTED_MODULE_2__["default"])());
         var result = [];
         result.push('P');
         for (var _i = 0, _a = this.units; _i < _a.length; _i++) {
@@ -228,7 +145,7 @@ var Duration = /** @class */ (function () {
         return result.join('');
     };
     Duration.prototype.toObject = function () {
-        return Object(_make_object__WEBPACK_IMPORTED_MODULE_1__["default"])(this.duration, this.units);
+        return (0,_make_object__WEBPACK_IMPORTED_MODULE_1__["default"])(this.duration, this.units);
     };
     Duration.SEC = 1000;
     Duration.MIN = Duration.SEC * 60;
@@ -238,7 +155,7 @@ var Duration = /** @class */ (function () {
     Duration.YEAR = Duration.DAY * 365;
     return Duration;
 }());
-/* harmony default export */ __webpack_exports__["default"] = (Duration);
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Duration);
 
 
 /***/ }),
@@ -247,12 +164,12 @@ var Duration = /** @class */ (function () {
 /*!*************************************!*\
   !*** ./src/duration/make-object.ts ***!
   \*************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return makeObject; });
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ makeObject)
+/* harmony export */ });
 /* harmony import */ var _calculate_months__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./calculate-months */ "./src/duration/calculate-months.ts");
 /* harmony import */ var _index__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index */ "./src/duration/index.ts");
 /* harmony import */ var _get_unit_list__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./get-unit-list */ "./src/duration/get-unit-list.ts");
@@ -260,7 +177,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 function makeObject(duration, units) {
-    if (units === void 0) { units = Object(_get_unit_list__WEBPACK_IMPORTED_MODULE_2__["default"])(); }
+    if (units === void 0) { units = (0,_get_unit_list__WEBPACK_IMPORTED_MODULE_2__["default"])(); }
     var YEAR = _index__WEBPACK_IMPORTED_MODULE_1__["default"].YEAR, WEEK = _index__WEBPACK_IMPORTED_MODULE_1__["default"].WEEK, DAY = _index__WEBPACK_IMPORTED_MODULE_1__["default"].DAY, HOUR = _index__WEBPACK_IMPORTED_MODULE_1__["default"].HOUR, MIN = _index__WEBPACK_IMPORTED_MODULE_1__["default"].MIN, SEC = _index__WEBPACK_IMPORTED_MODULE_1__["default"].SEC;
     var years = 0, months = 0, weeks = 0, days = 0, hours = 0, minutes = 0, seconds = 0;
     if (units.indexOf('years') > -1) {
@@ -268,7 +185,7 @@ function makeObject(duration, units) {
         duration -= years * YEAR;
     }
     if (units.indexOf('months') > -1) {
-        var monthsObj = Object(_calculate_months__WEBPACK_IMPORTED_MODULE_0__["default"])(duration);
+        var monthsObj = (0,_calculate_months__WEBPACK_IMPORTED_MODULE_0__["default"])(duration);
         months = monthsObj.months;
         duration = monthsObj.remaining;
     }
@@ -309,12 +226,12 @@ function makeObject(duration, units) {
 /*!**********************************************!*\
   !*** ./src/duration/process-units-option.ts ***!
   \**********************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return processUnitsOption; });
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ processUnitsOption)
+/* harmony export */ });
 function processUnitsOption(unitList, options) {
     var units = options.units;
     if (!units) {
@@ -341,12 +258,12 @@ function processUnitsOption(unitList, options) {
 /*!******************************************!*\
   !*** ./src/duration/trim-zero-values.ts ***!
   \******************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return trimZeroValues; });
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ trimZeroValues)
+/* harmony export */ });
 function trimZeroValues(units, duration, config) {
     if (!config.string) {
         return;
@@ -393,18 +310,74 @@ function trimZerosRight(units, duration) {
 }
 
 
-/***/ }),
+/***/ })
 
-/***/ "./src/index.ts":
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
 /*!**********************!*\
   !*** ./src/index.ts ***!
   \**********************/
-/*! exports provided: duration */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "duration", function() { return duration; });
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "duration": () => (/* binding */ duration)
+/* harmony export */ });
 /* harmony import */ var _duration_index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./duration/index */ "./src/duration/index.ts");
 
 var DEFAULT_CONFIG = {
@@ -429,9 +402,10 @@ function defaults(defaultConfig) {
     return bootstrappedDuration;
 }
 
+})();
 
-/***/ })
-
-/******/ });
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
 });
 //# sourceMappingURL=index.js.map
